fe61f5a0ad56ab4535037593d528dbc0a815c51d
gandibleux-x <xavier.gandibleux@univ-nantes.fr>
2021-05-26 09:08:07 +0200

    Create exercises2.jl

---
+++ exercises2.jl
@@ -0,0 +1,123 @@
+### A Pluto.jl notebook ###
+# v0.14.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 93650504-b182-11eb-33a4-0f811778f54b
+md"""
+## Lesson 2: Exercises
+-----
+"""
+
+# ╔═╡ 2d51a897-a921-4ba2-af9b-8d8fed80bde2
+md""" 
+*Remark*: for exercises 1.7 and 1.8, your program has to be prepared with your favorite editor and run into the REPL. Don't forget to comment your programs.
+"""
+
+# ╔═╡ 7bf03548-ad77-4cf0-b8fe-d37bee20a72f
+md"""
+###### 1.1 What are the characters that are used for strings?
+
+1. Single or double quote
+2. Double quotes or sets of three double quotes
+3. Only single quotes
+4. Only double quotes
+Try your answer into the REPL.
+"""
+
+# ╔═╡ bd0292e6-1ffc-4600-80e9-5aae1831aad8
+
+
+# ╔═╡ 7bc7852b-f55e-46ca-af76-a598974f17fc
+md"""
+###### 1.2 Look up the julia documentation for the `convert` function.
+"""
+
+# ╔═╡ ad7ca511-6f05-4f47-a074-0a3934448b02
+
+
+# ╔═╡ 71051549-7a53-4536-8f74-682962e2e53e
+md"""
+###### 1.3 Assign the value *24* to a variable named *hours*. Convert *hours* to a float and assign it to variable *hours_float*
+"""
+
+# ╔═╡ 36528f5a-f5f6-407d-b358-4b04f9306f62
+
+
+# ╔═╡ 929b71cd-f57e-4cab-b0e6-cb253e1b37c5
+md"""
+###### 1.4 See what happens when you execute
+1. `convert(Int64, "1")`
+2. `parse(Int64, "1")`
+3. `convert(Int64, 1.0)`
+4. `convert(Int64, "1.3")`
+"""
+
+# ╔═╡ 77580af7-71df-4da2-9015-4d9a0bed0bdf
+
+
+# ╔═╡ f33a96a2-2264-4620-9976-f8ba68c99281
+md"""
+###### 1.5 Write a program which read the first name of the user on the console and displays it as follow:
+```
+What's your name ? 
+
+Xavier
+
+The name is Xavier
+```
+"""
+
+# ╔═╡ 47f40a42-fa41-4074-a164-ac759fae4266
+
+
+# ╔═╡ 7bddbfe1-6e31-4b6b-a874-b65b11b37976
+md"""
+###### 1.6 For the exercise 1.5, display the type of the input
+"""
+
+# ╔═╡ 0df9cc0c-6eec-4df4-9c59-c9ce89dd1c4d
+
+
+# ╔═╡ c7b10600-fe5a-4664-ae8c-2249e6cb83a4
+md"""
+###### 1.7 Code a program which read two numerical data on the console, one integer and one float, and displays: 
+1. the sum of the numbers
+2. the type of the sum
+"""
+
+# ╔═╡ 1bca1206-2031-4581-b436-46a87c82c624
+
+
+# ╔═╡ ebebacca-468b-440a-8eee-d3a8fd2d56f7
+md"""
+###### 1.8 Code a program `hms.jl` which perform the following steps:
+1. Ask from the user an elapsed time in minutes (a float value);
+2. Assign this value to a variable named *time_elapsed*; 
+3. Convert *time_elapsed* into three integers named *hours*, *minutes*, *seconds*; 
+4. Display the results on the terminal. 
+"""
+
+# ╔═╡ df67bd63-3944-45a6-a0be-60ee2d7b53ad
+
+
+# ╔═╡ Cell order:
+# ╟─93650504-b182-11eb-33a4-0f811778f54b
+# ╟─2d51a897-a921-4ba2-af9b-8d8fed80bde2
+# ╟─7bf03548-ad77-4cf0-b8fe-d37bee20a72f
+# ╠═bd0292e6-1ffc-4600-80e9-5aae1831aad8
+# ╟─7bc7852b-f55e-46ca-af76-a598974f17fc
+# ╠═ad7ca511-6f05-4f47-a074-0a3934448b02
+# ╟─71051549-7a53-4536-8f74-682962e2e53e
+# ╠═36528f5a-f5f6-407d-b358-4b04f9306f62
+# ╟─929b71cd-f57e-4cab-b0e6-cb253e1b37c5
+# ╠═77580af7-71df-4da2-9015-4d9a0bed0bdf
+# ╟─f33a96a2-2264-4620-9976-f8ba68c99281
+# ╠═47f40a42-fa41-4074-a164-ac759fae4266
+# ╟─7bddbfe1-6e31-4b6b-a874-b65b11b37976
+# ╠═0df9cc0c-6eec-4df4-9c59-c9ce89dd1c4d
+# ╟─c7b10600-fe5a-4664-ae8c-2249e6cb83a4
+# ╠═1bca1206-2031-4581-b436-46a87c82c624
+# ╟─ebebacca-468b-440a-8eee-d3a8fd2d56f7
+# ╠═df67bd63-3944-45a6-a0be-60ee2d7b53ad
